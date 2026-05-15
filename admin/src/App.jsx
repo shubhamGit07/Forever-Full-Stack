3b9e80dcf0ed814ee161204398baf5e030039c57
Shubham <shubhamkumar6000ara@gmail.com>
2026-05-15 23:41:55 +0530

    first commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-export const backendUrl = "https://forever-backend-five-flame.vercel.app"
+export const backendUrl = "http://localhost:4000"
 
 export const currency = '$'
 
