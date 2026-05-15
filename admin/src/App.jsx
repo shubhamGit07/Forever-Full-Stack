d676007e94be4ec34ef1cbd40f504b55301bf933
Shubham <shubhamkumar6000ara@gmail.com>
2026-05-16 00:49:03 +0530

    fix admin backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = "https://forever-backend-five-flame.vercel.app"
 
 export const currency = '$'
 
