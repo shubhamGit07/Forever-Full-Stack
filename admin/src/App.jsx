06ecabd19b01bf6ad1b9900d06b0b4ed0c0b2022
Shubham <shubhamkumar6000ara@gmail.com>
2026-05-16 00:44:12 +0530

    fix admin backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-export const backendUrl = "http://localhost:4000"
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 export const currency = '$'
 
